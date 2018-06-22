de29215f772bd1b0c73726ac8fe031a4f8ad639b
Caleb Fahlgren <cfahlgren1@gmail.com>
2018-06-21 23:25:43 -0400

    added banner, and added more data gathering

--- crawl.py
+++ crawl.py
@@ -1,4 +1,37 @@
-import os,grp, time
+import os, grp, time, sys
+from time import sleep
+
+print (" " + """
+
+############################################
+  ___          _                             
+ / _ \        | |                            
+/ /_\ \ _   _ | |__   _   _  _ __  _ __      
+|  _  || | | || '_ \ | | | || '__|| '_ \     
+| | | || |_| || |_) || |_| || |   | | | |    
+\_| |_/ \__,_||_.__/  \__,_||_|   |_| |_|    
+ _____  _               _               _    
+/  ___|| |             | |             | |   
+\ `--. | |_  _   _   __| |  ___  _ __  | |_  
+ `--. \| __|| | | | / _` | / _ \| '_ \ | __| 
+/\__/ /| |_ | |_| || (_| ||  __/| | | || |_  
+\____/  \__| \__,_| \__,_| \___||_| |_| \__| 
+ _____                                       
+/  ___|                                      
+\ `--.   ___  __ _  _ __   _ __    ___  _ __ 
+ `--. \ / __|/ _` || '_ \ | '_ \  / _ \| '__|
+/\__/ /| (__| (_| || | | || | | ||  __/| |   
+\____/  \___|\__,_||_| |_||_| |_| \___||_|  
+ 
+
+############################################
+                                             
+                                             
+""")
+
+time.sleep(0.5)
+
+
 
 # Change directory to root home
 os.chdir("/home")
@@ -7,7 +40,8 @@ os.chdir("/home")
 dirList = ['new-ece', 'u1', 'u2', 'u3', 'che_h2', 'cse_h1', 'cse_h2']
 
 # Ignore these common system directories
-ignoreDirList = ['Connections', 'UserData', '$RECYCLE.BIN', '.config', '.local', '.ssh', '.wastebasket', '.fm',
+ignoreDirList = ['.evolution', '.matlab', '.nautilus', '.mozilla', '.felix', 'Connections', 'UserData', '$RECYCLE.BIN',
+                 '.config', '.local', '.ssh', '.wastebasket', '.fm',
                  '.netscape-cache', '.login', '.logout', '.Xdefaults', 'Desktop', 'Documents', 'Application Data',
                  'appdata', 'bash_history', '.cache', '.openwin-init', 'Startup', 'Accessories', 'Programs',
                  'Administrative Tools', 'Libraries', 'SendTo', 'Start Menu', 'Themes', 'AccountPictures', 'Recent',
@@ -29,21 +63,21 @@ ignoreFileList = ['.fm', 'wastebasket', '.login', '.Xdefaults', 'ntuser.cmd', 'B
 # Counts on different user and types
 incrementer = 0
 
-newece_count = 0 # Count of users in new-ece directory
-u1_count_total = 0 # Count of users in u1 directory
-u2_count_total = 0 # Count of users in u2 directory
-u3_count_total = 0 # Count of users in u3 directory
-cse_h2_count_total = 0 # Count of users in cse_h2
-cse_h1_count_total = 0 # Count of users in cse_h1
-che_h2_count_total = 0 # Count of users in che_h2
+newece_count = 0  # Count of users in new-ece directory
+u1_count_total = 0  # Count of users in u1 directory
+u2_count_total = 0  # Count of users in u2 directory
+u3_count_total = 0  # Count of users in u3 directory
+cse_h2_count_total = 0  # Count of users in cse_h2
+cse_h1_count_total = 0  # Count of users in cse_h1
+che_h2_count_total = 0  # Count of users in che_h2
 
-active_users = 0 # Count of users that have created files
-total_file_count = 0 # Count files in total
-acc_file_count = 0 # Count files that are readable
+active_users = 0  # Count of users that have created files
+total_file_count = 0  # Count files in total
+acc_file_count = 0  # Count files that are readable
 
-neng_count = 0 # Count users in neng group
-ee_count = 0 # Count users in ee group
-ugrad_count = 0 # Count users in ugrad group
+neng_count = 0  # Count users in neng group
+ee_count = 0  # Count users in ee group
+ugrad_count = 0  # Count users in ugrad group
 
 for directory in dirList:
     t0 = time.time()
@@ -51,81 +85,94 @@ for directory in dirList:
     try:
         os.chdir(temp_path)
         for user_dirs in os.listdir(temp_path):
+            if (user_dirs not in ignoreDirList):
+                # Count how many users are in each directory
+                if (directory == "u1"): u1_count_total += 1
+                if (directory == "u2"): u2_count_total += 1
+                if (directory == "u3"): u3_count_total += 1
+                if (directory == "new-ece"): newece_count += 1
+                if (directory == "cse_h1"): cse_h1_count_total += 1
+                if (directory == "cse_h2"): cse_h2_count_total += 1
+                if (directory == "che_h2"): cse_h2_count_total += 1
 
-            # Count how many users are in each directory
-            if (directory == "u1"): u1_count_total += 1
-            if (directory == "u2"): u2_count_total += 1
-            if (directory == "u3"): u3_count_total += 1
-            if (directory == "new-ece"): newece_count += 1
-            if (directory == "cse_h1"): cse_h1_count_total += 1
-            if (directory == "cse_h2"): cse_h2_count_total += 1
-            if (directory == "che_h2"): cse_h2_count_total += 1
+                # Variable to count number of files
+                numOfFiles = 0
+                numOfAccFiles = 0
+                numOfWritable = 0
 
-            # Variable to count number of files
-            numOfFiles = 0
-            numOfAccFiles = 0
+                # Set the path to change the directory
+                user_path = (temp_path + user_dirs + "/")
 
-            # Set the path to change the directory
-            user_path = (temp_path + user_dirs + "/")
+                # User Count Incrementer
+                incrementer += 1
 
-            # User Count Incrementer
-            incrementer += 1
+                # Change Directory to User Folder and catch errors
+                try:
+                    os.chdir(user_path)
 
-            #Change Directory to User Folder and catch errors
-            try:
-                os.chdir(user_path)
+                    stat_info = os.stat(user_path)
+                    uid = stat_info.st_uid
+                    gid = stat_info.st_gid
 
-                stat_info = os.stat(user_path)
-                uid = stat_info.st_uid
-                gid = stat_info.st_gid
+                    if (str(grp.getgrgid(gid)[0]) == "ee"):
+                        ee_count += 1
+                    if (str(grp.getgrgid(gid)[0]) == "neng"):
+                        neng_count += 1
+                    if (str(grp.getgrgid(gid)[0]) == "ugrad"):
+                        ugrad_count += 1
 
-                if (str(grp.getgrgid(gid)[0]) == "ee"):
-                    ee_count += 1
-                if (str(grp.getgrgid(gid)[0]) == "neng"):
-                    neng_count += 1
-                if (str(grp.getgrgid(gid)[0]) == "ugrad"):
-                    ugrad_count += 1
+                except Exception:
+                    continue
 
-            except Exception:
-                continue
+                # Crawl through user directory and count files
+                for roots, dirs, files in os.walk(user_path):
+                    # Count all files
+                    for file in files:
+                        # Check file against common system file list
+                        if file not in ignoreFileList:
+                            try:
 
-            # Crawl through user directory and count files
-            for roots, dirs, files in os.walk(user_path):
-                # Count all files
-                for file in files:
-                    # Check file against common system file list
-                    if file not in ignoreFileList:
-                        try:
-                            check_access_file = open(file, "r")
-                            check_access_file.close()
-                            acc_file_count += 1
-                            numOfAccFiles += 1
-                        except Exception:
+                                # Check Different Accesses
+                                check_access_file = os.access(file, os.R_OK)
+                                check_x_access = os.access(file, os.X_OK)
+                                check_w_access = os.access(file, os.W_OK)
+
+                                # Check Read and Execute Access Permissions
+                                if (check_access_file == True or check_x_access == True):
+                                    acc_file_count += 1
+                                    numOfAccFiles += 1
+                                # Check Write Access
+                                if (check_w_access == True):
+                                    numOfWritable += 1
+
+                            except Exception:
                                 pass
-                        numOfFiles += 1
-                        total_file_count += 1
+                            numOfFiles += 1
+                            total_file_count += 1
 
-            if (numOfFiles >= 1):
-                active_users += 1
+                if (numOfFiles >= 1):
+                    active_users += 1
 
-            print (str(incrementer) + "\t USER: " + user_dirs + "\tPATH: " + user_path + "\tNumOfFiles: " + str(numOfFiles) + "\tGROUP: " + str(grp.getgrgid(gid)[0]) + "\tAccessible Files: " + str(numOfAccFiles))
+                print(str(incrementer) + "\t USER: " + user_dirs + "\tPATH: " + user_path + "\tNumOfFiles: " + str(
+                    numOfFiles) + "\tGROUP: " + str(grp.getgrgid(gid)[0]) + "\tAccessible Files: " + str(
+                    numOfAccFiles) + "\tWritable: " + str(numOfWritable))
 
     # Catch Permission Denied Errors when opening directories
     except Exception():
         pass
 
-print ("\n\nTOTAL COUNT:  " + str(incrementer))
-print ("ACTIVE USERS   :  " + str(active_users))
-print ("TOTAL FILE COUNT: " + str(total_file_count))
-print ("ACCESSIBLE FILES: " + str(acc_file_count))
-print ("new-ece count:    " + str(newece_count))
-print ("u1 total count:   " + str(u1_count_total))
-print ("u2 total count:   " + str(u2_count_total))
-print ("u3 total count:   " + str(u3_count_total))
-print ("cse_h1 total count: " + str(cse_h1_count_total))
-print ("cse_h2 total count: " + str(cse_h2_count_total))
-print ("che_h2 total count: " + str(che_h2_count_total))
-print ("ugrad group count: " + str(ugrad_count))
-print ("neng group count : " + str(neng_count))
-print ("ee group count   : " + str(ee_count))
+print("\n\nTOTAL COUNT:  " + str(incrementer))
+print("ACTIVE USERS   :  " + str(active_users))
+print("TOTAL FILE COUNT: " + str(total_file_count))
+print("ACCESSIBLE FILES: " + str(acc_file_count))
+print("new-ece count:    " + str(newece_count))
+print("u1 total count:   " + str(u1_count_total))
+print("u2 total count:   " + str(u2_count_total))
+print("u3 total count:   " + str(u3_count_total))
+print("cse_h1 total count: " + str(cse_h1_count_total))
+print("cse_h2 total count: " + str(cse_h2_count_total))
+print("che_h2 total count: " + str(che_h2_count_total))
+print("ugrad group count: " + str(ugrad_count))
+print("neng group count : " + str(neng_count))
+print("ee group count   : " + str(ee_count))
 
